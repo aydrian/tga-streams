827ae0c76a6e7b4806f3ae23bd715fb74fdce7a2
Aydrian Howard <aydrian@gmail.com>
2021-12-17 08:47:53 -0500

    added configurable canvas for emote drop

--- pages/emotedrop/[twitchUser]/[emotes].js
+++ pages/emotedrop/[twitchUser]/[emotes].js
@@ -1,17 +1,24 @@
 import { ChatProvider } from "@context/chat";
 import { EmoteDrop } from "@components/EmoteDrop";
 
-export default function EmoteDropOverlay({ twitchUser, emotes }) {
+export default function EmoteDropOverlay({ twitchUser, emotes, canvas }) {
   return (
     <ChatProvider channels={[twitchUser]}>
-      <EmoteDrop filter={emotes} />
+      <EmoteDrop canvas={canvas} filter={emotes} />
     </ChatProvider>
   );
 }
 
 export async function getServerSideProps(context) {
-  const { twitchUser, emotes: strEmotes = "" } = context.query;
+  const {
+    twitchUser,
+    emotes: strEmotes = "",
+    canvas: strCanvas = ""
+  } = context.query;
   const emotes = strEmotes.length > 0 ? strEmotes.split(",") : ["CorgiDerp"];
+  const [width, height] =
+    strCanvas.length > 0 ? strCanvas.split("x") : ["1920", "1080"];
+  const canvas = { width: parseInt(width), height: parseInt(height) };
 
-  return { props: { twitchUser, emotes } };
+  return { props: { twitchUser, emotes, canvas } };
 }
