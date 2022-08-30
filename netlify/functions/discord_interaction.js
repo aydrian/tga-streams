77c00bac82e7a57082292cfa9b7637b9d8290acb
Aydrian Howard <aydrian@gmail.com>
2022-08-30 15:52:38 -0400

    basic interaction webhook function

---
+++ netlify/functions/discord_interaction.js
@@ -0,0 +1,36 @@
+import { InteractionResponseType, InteractionType } from "discord-interactions";
+
+import withVerifyDiscord from "netlify/lib/withVerifyDiscord";
+
+async function discordHandler(event, _context) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      headers: { Allow: "POST" },
+      body: "Method Not Allowed"
+    };
+  }
+
+  const interaction = JSON.parse(event.body);
+
+  if (interaction.type === InteractionType.PING) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: InteractionResponseType.PONG
+      })
+    };
+  } else if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `You used: ${interaction.data.name}`
+        }
+      })
+    };
+  }
+}
+
+export const handler = withVerifyDiscord(discordHandler);
